00341bbbf7b2dee5cb34ef7c64cbe8af6777b266
unknown <micofahrizal2019@gmail.com>
2024-08-22 13:39:54 +0700

    push 1

--- src/com/dicoding/javafundamental/basic/Main.java
+++ src/com/dicoding/javafundamental/basic/Main.java
@@ -1,4 +1,60 @@
 package com.dicoding.javafundamental.basic;
 
 public class Main {
+    public static void main (String[] args){
+        String salam = "Hallo Gaes";
+        System.out.println(salam);
+
+        //Menyimpan variable char lebih dari 1 karakter
+        char[] micoChars = {'m', 'i', 'c', 'o'};
+
+        //Membuat variable baru dari variable yang sudah ada
+        String micoString= new String(micoChars);
+        System.out.println(micoString);
+
+        //Mengetahui panjang string
+        int panjangString = micoString.length();
+        System.out.println(panjangString);
+
+        //Mengambil karakter tertentu (1 Karakter)
+        String ambil = "Mico Fahrizal";
+        char result =  ambil.charAt(8);
+        System.out.println(result);
+
+        //Mengambil karater tertentu (lebih dari 1 karakter)
+        //hitungan karakter seperti array mulai dari index ke 0
+        String ambil2 = "Bandar Lampung";
+        String hasil = ambil2.substring(0,5);
+        System.out.println(hasil);
+
+        String nama = "Andi";
+        int umur = 25;
+        double tinggiBadan = 1.75;
+
+        // Contoh 1: Menyisipkan string dan integer
+        String kalimat1 = String.format("Nama saya %s dan umur saya %d tahun.", nama, umur);
+        System.out.println(kalimat1);
+
+        // Contoh 2: Menyisipkan string, integer, dan double
+        String kalimat2 = String.format("Tinggi badan %s adalah %.2f meter.", nama, tinggiBadan);
+        System.out.println(kalimat2);
+
+        // Contoh 3: Menggunakan placeholder dengan nomor
+        String kalimat3 = String.format("Nilai ujian Matematika: %1$d, Bahasa Inggris: %2$d", 85, 90);
+        System.out.println(kalimat3);
+
+
+        // Menggunakan contains() untuk mencari sebuah karakter pada vsebuah string
+        String kalimat = "Ini adalah contoh kalimat";
+        String kataYangDicari = "contoh";
+
+        boolean apakahTerdapat = kalimat.contains(kataYangDicari);
+
+        if (apakahTerdapat) {
+            System.out.println("Kalimat mengandung kata '" + kataYangDicari + "'");
+        } else {
+            System.out.println("Kalimat tidak mengandung kata '" + kataYangDicari + "'");
+        }
+
+    }
 }
