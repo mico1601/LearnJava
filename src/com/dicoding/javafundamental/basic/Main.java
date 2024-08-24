bf2dde5bca0fcc9169ae087c50dd3f58e22ede1b
unknown <micofahrizal2019@gmail.com>
2024-08-24 10:34:22 +0700

    commit pertama

--- src/com/dicoding/javafundamental/basic/Main.java
+++ src/com/dicoding/javafundamental/basic/Main.java
@@ -4,45 +4,51 @@ public class Main {
     public static void main (String[] args){
         String salam = "Hallo Gaes";
         System.out.println(salam);
-
+//        output = Hallo Gaes
         //Menyimpan variable char lebih dari 1 karakter
+
         char[] micoChars = {'m', 'i', 'c', 'o'};
 
         //Membuat variable baru dari variable yang sudah ada
         String micoString= new String(micoChars);
         System.out.println(micoString);
+        // output = mico
 
         //Mengetahui panjang string
         int panjangString = micoString.length();
         System.out.println(panjangString);
+        // output 4
 
         //Mengambil karakter tertentu (1 Karakter)
         String ambil = "Mico Fahrizal";
         char result =  ambil.charAt(8);
         System.out.println(result);
+        //output = r
 
         //Mengambil karater tertentu (lebih dari 1 karakter)
         //hitungan karakter seperti array mulai dari index ke 0
         String ambil2 = "Bandar Lampung";
         String hasil = ambil2.substring(0,5);
         System.out.println(hasil);
+        //output = Banda
 
         String nama = "Andi";
         int umur = 25;
         double tinggiBadan = 1.75;
-
         // Contoh 1: Menyisipkan string dan integer
         String kalimat1 = String.format("Nama saya %s dan umur saya %d tahun.", nama, umur);
         System.out.println(kalimat1);
+        //output = Nama saya Andi dan umur saya 25 tahun.
 
         // Contoh 2: Menyisipkan string, integer, dan double
         String kalimat2 = String.format("Tinggi badan %s adalah %.2f meter.", nama, tinggiBadan);
         System.out.println(kalimat2);
+        //output = Tinggi badan Andi adalah 1.75 meter.
 
         // Contoh 3: Menggunakan placeholder dengan nomor
         String kalimat3 = String.format("Nilai ujian Matematika: %1$d, Bahasa Inggris: %2$d", 85, 90);
         System.out.println(kalimat3);
-
+        //output = Nilai ujian Matematika: 85, Bahasa Inggris:90
 
         // Menggunakan contains() untuk mencari sebuah karakter pada vsebuah string
         String kalimat = "Ini adalah contoh kalimat";
@@ -55,6 +61,6 @@ public class Main {
         } else {
             System.out.println("Kalimat tidak mengandung kata '" + kataYangDicari + "'");
         }
-
+        // output = Kalimat mengandung kata 'contoh'
     }
 }
